200efa262337a44973bada659a2db67ab8349b1f
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-04-18 16:14:01 +0300

    init structure

---
+++ src/main/java/com/paperfox/order/OrderApplication.java
@@ -0,0 +1,15 @@
+package com.paperfox.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+		System.out.println("Hello");
+
+	}
+
+}
